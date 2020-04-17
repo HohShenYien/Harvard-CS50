18745ddc437d897d9104c07cca7b6fd651bf5afa
HohShenYien <55322546+HohShenYien@users.noreply.github.com>
2020-04-17 14:36:36 +0800

    Create moreMario.c

---
+++ Week1/moreMario.c
@@ -0,0 +1,51 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_positive_int_less_than(int x);
+void build_pyramid(int x);
+
+int main(void)
+{
+    int n = get_positive_int_less_than(8);
+    build_pyramid(n);
+}
+
+//this function will prompt for input
+//until a positive number less than x is give
+int get_positive_int_less_than(int x)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > x);
+    
+    return n;
+}
+
+//this function builds the pyramid
+void build_pyramid(int x)
+{
+    //first loop for every row
+    for (int i = 1; i <= x; i++)
+    {
+        //loop for the space
+        for (int k = x; k > i; k--)
+        {
+            printf(" ");
+        }
+        //loop for the left part the pyramid
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        //loop for the right part for the pyramid
+        for (int m = 0; m < i; m++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
