d841b0c22d824c120521a47c811ddcd33a51f0c1
HohShenYien <55322546+HohShenYien@users.noreply.github.com>
2020-06-04 11:36:57 +0800

    Create mario_more.py

---
+++ Week6/mario_more.py
@@ -0,0 +1,29 @@
+def main():
+    num = get_int()  # Getting the input
+    for x in list("#" * i for i in range(1, num + 1)):
+        # rjust makes sure the string is width num else it shifts the whole string to the right
+        # this prints the left side
+        print(x.rjust(num), end="")
+        # 2 spaces in between
+        print("  ", end="")
+        # print the remaining right side
+        print(x)
+
+
+def get_int():
+    temp = True
+    # Repeat until the input is valid integer and between 1 and 8 inclusive
+    while temp:
+        try:
+            # try will run the function and stop if it fails
+            num = int(input("Height: "))
+            # If within the range
+            if not (num < 1 or num > 8):
+                temp = False
+        except:
+            # Rerun the while loop if not and integer
+            pass
+    return num
+
+
+main()
